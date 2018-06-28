b9f926e1cda2167bd4555b2f598bc1236bc7f736
pixies <pixies@riseup.net>
2018-06-28 14:16:20 +0000

    solving accounts urls for login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,14 +7,19 @@ from rest_framework.urlpatterns import format_suffix_patterns
 #from accounts.views import UserList, UserDetail, UserCreate
 from accounts.api.views import UserCreateAPIView, UserListAPIView, UserDetailAPIView
 
-#from .views import login, search
+from .views import login, profile
+
+app_name = 'accounts'
 
 urlpatterns = [
 
     #url(r'^l/', login),
     # Users
     # path api/users/
-    url('^', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    url(r'^profile/$', profile),
+    #url('^login/$', login, name='login')
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    url(r'^logout/$', auth_views.LogoutView.as_view()),  # API Auth - URLs
 #    url('^change-password/$', auth_views.PasswordChangeView.as_view(template_name='change-password.html'))
 
 ]
