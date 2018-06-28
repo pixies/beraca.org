0f71438e159502ed50af1d03ab9fe09ec0ee0205
pixies <pixies@riseup.net>
2018-06-28 14:25:46 +0000

    add methods login and profile

--- accounts/views.py
+++ accounts/views.py
@@ -52,8 +52,98 @@ class UserDetail(mixins.RetrieveModelMixin,
 
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
-def my_login(request):
-    return HttpResponse('Login lll')
+def login(request):
+
+    print(request.user)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        print(form)
+        if form.is_valid():
+            return redirect('/index/search/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form':form})
+
+
+def profile(request):
+
+
+    from accounts.models import MyUser
+    from client.models import Profile, Client
+    from project.models import ProjectcSupport
+    from company.models import Company
+
+    cliente_id = request.user.id
+    conta = MyUser.objects.get(pk=request.user.id)
+    print(cliente_id)
+    profile, created = Profile.objects.get_or_create(user_id=cliente_id, is_support=False, is_manager=False, is_admin=False)
+
+    nascimento = ''
+    endereco = ''
+    
+    if created == True:
+        if profile.birth_date.exists():
+            nascimento = profile.birth_date
+            
+        else:
+            nascimento = ''
+
+        if profile.address.exists():
+            endereco = profile.address
+        else:
+            endereco = ''
+
+    cliente = Client.objects.get(user_id=cliente_id)
+
+    projetcSupport = ProjectcSupport.objects.filter(client_id=cliente_id)
+
+    print(projetcSupport)
+    print(cliente)
+    print(profile)
+    
+    full_name = str(conta.first_name) + ' ' + str(conta.last_name)
+    email = str(conta.email)
+    fone = str(conta.phone)
+
+    for pro in projetcSupport:
+        print(pro.project.name)
+
+    context = {
+        'full_name': full_name,
+        'email': email,
+        'nascimento': nascimento,
+        'endereco': endereco,
+        'fone': fone,
+
+        'company': cliente.company,
+        'cnpj': cliente.company.company_reg,
+
+        'projetos': projetcSupport
+    }
+
+    return render(request, 'profile.html', context=context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
